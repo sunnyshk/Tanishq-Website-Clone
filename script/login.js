13a4bf100b419183f91b55023b49a50b12b2ea36
Shubham Dubey <shubhamdubey.dubey4@gmail.com>
2022-02-26 14:39:28 +0530

    final push

--- script/login.js
+++ script/login.js
@@ -10,16 +10,19 @@ var loggedInUser = [];
 function signin(){
   var email = document.getElementById("email1rth").value;
   var pass = document.getElementById("pass1rth").value;
+
+  
   
   for(var a = 0; a<regdUsers.length; a++){
-      if(regdUsers[a].emailAdd == email && regdUsers[a].passwd == pass){
+      if(regdUsers[a].emailAdd === email && regdUsers[a].passwd === pass){
           localStorage.setItem('name',JSON.stringify(regdUsers[a].name))
-          window.location.href = "home.html";
+          
           alert("You Can Shop Now With Best Offers!");
          
          loggedInUser.push(regdUsers[a]);
          localStorage.setItem("logInUserdata", JSON.stringify(loggedInUser));
-         continue;
+         window.location.href = "home.html";
+         break;
 
       }
      
